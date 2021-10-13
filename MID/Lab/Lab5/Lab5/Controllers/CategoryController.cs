29e2d5965e3bf2c446cf42623cbc9fb41e27a17a
Mashwd <mdmohaimen3@gmail.com>
2021-10-13 14:17:18 +0600

    Entity framework: Category added to the product

---
+++ MID/Lab/Lab5/Lab5/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Lab5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab5.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var db = new UMSEntities();
+            var categories = db.Categories.ToList();
+            return View(categories);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var db = new UMSEntities();
+            var data = (from d in db.Categories
+                        where d.Id == id
+                        select d).FirstOrDefault();
+            return View(data);
+        }
+    }
+}
